56d311c9931b7a6dea9d689216619cef53fbfbb0
vi999z <godwinavena3@gmail.com>
2026-07-25 23:10:43 +0800

    silly mistake

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+  baseURL: import.meta.env.VITE_API_URL || 'https://medihub-1-jx4l.onrender.com'
 });
 
 api.interceptors.request.use((config) => {
